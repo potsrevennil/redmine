7519678acae817a8c5b582d69c55c68cca9c4405
potsrevennil <b02901144@ntu.edu.tw>
2016-07-13 10:11:05 +0900

    modified coding style of parse.js

--- src/parse.js
+++ src/parse.js
@@ -11,47 +11,44 @@ const stream = csv({
   headers: ['usr', 'status', 'time', 'agent', 'foo', 'bar'],
 });
 
-var vData = [];
-var trace = {
+const vData = [];
+const trace = {
   x: [],
   y: [],
-  fill: "tozeroy",
-  type: "scatter"
+  fill: 'tozeroy',
+  type: 'scatter',
 };
 
 
-var parse = fs.createReadStream('../EVENTS_20160601.log')
+fs.createReadStream('../EVENTS_20160601.log')
   .pipe(stream)
   .on('data', (data) => {
-    var vecDate = data.time.split(" ")[0].split("/");
-    var vecTime = data.time.split(" ")[1].split(":");
-    var d = {
-      'usr': data.usr,
-      'status': data.status,
-      'year': vecDate[0],
-      'month': vecDate[1],
-      'day': vecDate[2],
-      'hr': vecTime[0],
-      'min': vecTime[1],
-      'agent': data.agent,
-      'foo': data.foo,
-      'bar': data.bar,
+    const vecDate = data.time.split(' ')[0].split('/');
+    const vecTime = data.time.split(' ')[1].split(':');
+    const d = {
+      usr: data.usr,
+      status: data.status,
+      year: vecDate[0],
+      month: vecDate[1],
+      day: vecDate[2],
+      hr: vecTime[0],
+      min: vecTime[1],
+      agent: data.agent,
+      foo: data.foo,
+      bar: data.bar,
     };
-    
     vData.push(d);
-    var t = Number(d.hr) + Number(d.min) / 60;
+    const t = Number(d.hr) + Number(d.min) / 60;
     if (trace.x.length === 0 || trace.x[trace.x.length - 1] !== t) {
       trace.x.push(t);
       trace.y.push(1);
-    }
-    else {
-    //else if (trace[trace.length - 1] === t){
+    } else {
       trace.y[trace.y.length - 1] += 1;
     }
   })
   .on('end', () => {
-    var graphOptions = {filename: "event_20160601", fileopt: "overwrite"};
-    plotly.plot(trace, graphOptions, function (err, msg) {
-          console.log(msg);
+    const graphOptions = { filename: 'event_20160601.1', fileopt: 'overwrite' };
+    plotly.plot(trace, graphOptions, (err, msg) => {
+      console.log(msg);
     });
   });
