9d4512849a9d08797f59953c438b755f33d1d259
potsrevennil <b02901144@ntu.edu.tw>
2016-07-15 11:33:11 +0900

    Sort data, prepare data for chart

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import 'babel-polyfill';
-import { polyfill } from 'es6-promise';
 import fetch from 'isomorphic-fetch';
+import React from 'react';
+import { render } from 'react-dom';
 
+
+var  cData = [];
 fetch('/api')
   .then(res => res.json())
   .then(data => {
-    console.log(data);
-  })
+    function compare(d1, d2) {
+      const t1 = new Date(d1.time).getTime();
+      const t2 = new Date(d2.time).getTime();
+      if (t1 > t2)
+        return 1;
+      else if (t1 < t2)
+        return -1;
+      else 
+        return 0;
+    }
+    
+    data.sort(compare);
+
+    data.forEach( (d) => {
+      if (cData.length === 0 || d.time !== cData[cData.length - 1].time) {
+        cData.push( {time: d.time, amount: 1} );
+      }
+      else {
+        cData[cData.length - 1].amount += 1;
+      }
+    });
+  });
